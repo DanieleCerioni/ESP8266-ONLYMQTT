877d356431b55e9a5d64eb34254d3dd8a5e8d373
DanieleCerioni <44276019+DanieleCerioni@users.noreply.github.com>
2020-03-06 20:55:10 +0100

    Add files via upload

---
+++ wifi.ino
@@ -0,0 +1,52 @@
+#include <BearSSLHelpers.h>
+#include <CertStoreBearSSL.h>
+#include <ESP8266WiFi.h>
+#include <ESP8266WiFiAP.h>
+#include <ESP8266WiFiGeneric.h>
+#include <ESP8266WiFiMulti.h>
+#include <ESP8266WiFiScan.h>
+#include <ESP8266WiFiSTA.h>
+#include <ESP8266WiFiType.h>
+#include <WiFiClient.h>
+#include <WiFiClientSecure.h>
+#include <WiFiClientSecureAxTLS.h>
+#include <WiFiClientSecureBearSSL.h>
+#include <WiFiServer.h>
+#include <WiFiServerSecure.h>
+#include <WiFiServerSecureAxTLS.h>
+#include <WiFiServerSecureBearSSL.h>
+#include <WiFiUdp.h>
+
+#include <dummy.h>
+
+#define LED 16  //piedino GPIO16 ----->>> D0
+
+
+
+void setup()
+{
+  Serial.begin(115200);
+  Serial.println();
+
+  pinMode(LED, OUTPUT);
+  
+  WiFi.begin("GruppoPSC", "protectionUPSC270");
+
+  Serial.print("Connecting");
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println();
+
+  Serial.print("Connected, IP address: ");
+  Serial.println(WiFi.localIP());
+  digitalWrite(LED, HIGH);
+  
+}
+
+void loop() {
+
+  
+  }
